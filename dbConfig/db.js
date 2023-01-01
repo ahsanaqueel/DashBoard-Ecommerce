1bfa468dbb08a60050e43b0b8e83b7294a96b85f
ahsanaqueel <34808286+ahsanaqueel@users.noreply.github.com>
2023-01-01 15:32:03 +0500

    Update db.js

--- dbConfig/db.js
+++ dbConfig/db.js
@@ -3,16 +3,14 @@ require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 const mongoose = require('mongoose');
 const DB = process.env.DATABASE_URI;
 
-const connectDB= async()=>{
-  await   mongoose.connect(DB, (err, connection)=>{
-    'mongodb://localhost:27017/EccomerceStoreDB'
-   if(connection){
-       console.log("Database Connected");
-   }
-   else{
-       console.log(err);
-   }
-   })
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(DB);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
 }
 
 module.exports = connectDB;
